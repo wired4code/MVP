8d1990ae7357fcc413ac60e0b03e1c71c51d4caf
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-15 18:31:46 -0200

    Add controller, scope displaying on index.html

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var app = angular.module('myApp', ['ngRoute', 'ngResource']);
+
+app.controller('currentWeatherController', ['$scope', function($scope){
+
+  $scope.city = 'Ann Arbor';
+  $scope.temp = 50;
+
+}]);
