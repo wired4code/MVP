f36015f1845f50e30673c76553b75c6f136b9144
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-15 21:55:58 -0200

    Add custom zipcode service to bind data typed in input field

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var app = angular.module('myApp', ['ngRoute', 'ngResource']);
 
+app.service('zipService', function(){
+  this.zip = 22201;
+});
+
 
 // controller
-app.controller('currentWeatherController', ['$scope', '$resource', function($scope, $resource){
+app.controller('currentWeatherController', ['$scope', '$resource', 'zipService', function($scope, $resource, zipService){
 
-  $scope.zipCode = '';
+  $scope.zip = zipService.zip;
 
-  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather?zip=48197,us&appid=d6dbfbaff7932cf4fe546adbf96d084d", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+  $scope.$watch('zip', function(){
+    zipService.zip = $scope.zip;
+  })
 
-  $scope.result = $scope.weatherAPI.get({name:$scope.city})
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather?zip="+$scope.zip+",us&appid=d6dbfbaff7932cf4fe546adbf96d084d", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+  $scope.result = $scope.weatherAPI.get(/*{name:$scope.city}*/)
 
     console.log($scope.result);
 }]);
