505b75786447ea4aea1b24fc5d00f5f7edba911b
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-15 20:05:10 -0200

    Add openWeatherApp.org API

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 var app = angular.module('myApp', ['ngRoute', 'ngResource']);
 
-// services
-
-/*app.service('zipService', function(){
-  this.zip = '48104';
-});*/
 
 // controller
-app.controller('currentWeatherController', ['$scope', /*'zipService', */function($scope, zipService){
+app.controller('currentWeatherController', ['$scope', '$resource', function($scope, $resource){
 
   $scope.zipCode = '';
 
-/*  $scope.$watch('zip', function(){
-    zipService.zip = $scope.zip;
-  });*/
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather?zip=48197,us&appid=d6dbfbaff7932cf4fe546adbf96d084d", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
+  $scope.result = $scope.weatherAPI.get({name:$scope.city})
+
+    console.log($scope.result);
 }]);
 
 
